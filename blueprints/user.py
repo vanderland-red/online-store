ea91b3aae70481e5915fe758a5cdc6eaeaee7fbe
Mohammadreza <mohammad.za9297@gmail.com>
2026-06-03 17:14:19 +0330

    table cart & payment added

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for,flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, login_required
 from models.tables import User
 from extentions import db
 
--- models/tables.py
+++ models/tables.py
@@ -1,9 +1,10 @@
-from extentions import db
 from flask_login import UserMixin
+from extentions import db
+
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False, index=True)
     password = db.Column(db.String(255), nullable=False)
@@ -11,13 +12,12 @@ class User(db.Model, UserMixin):
     address = db.Column(db.Text, nullable=True)
 
     def __repr__(self):
-        return f'<User {self.username}>'
-
+        return f"<User {self.username}>"
 
 
 class Product(db.Model):
     __tablename__ = 'products'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False, index=True)
     description = db.Column(db.Text, nullable=True)
@@ -25,4 +25,39 @@ class Product(db.Model):
     active = db.Column(db.Boolean, default=True, nullable=False)
 
     def __repr__(self):
-        return f'<Product {self.name}>'
+        return f"<Product {self.name}>"
+
+
+class Cart(db.Model):
+    __tablename__ = 'carts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(50), nullable=False)
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        nullable=False
+    )
+
+    user = db.relationship("User", backref="carts")
+
+    def __repr__(self):
+        return f"<Cart id={self.id} status={self.status}>"
+
+
+class Payment(db.Model):
+    __tablename__ = 'payments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(50), nullable=False)
+
+    cart_id = db.Column(
+        db.Integer,
+        db.ForeignKey('carts.id'),
+        nullable=False
+    )
+    cart = db.relationship("Cart", backref="payments")
+
+    def __repr__(self):
+        return f"<Payment id={self.id} status={self.status}>"
